2d6d343e55671613fe29d22527ca579f7404151b
dxomaster <eden97.bar@gmail.com>
2020-06-02 15:35:40 +0300

    Fixes

--- BandwithMonitor/main.py
+++ BandwithMonitor/main.py
@@ -12,47 +12,67 @@ ASCII_INTRO = """    ____                  __         _ __  __       __  ___    
                                                                                        
 
 """
-os.system('cls')
 print(ASCII_INTRO)
-time.sleep(1.5)
-run = True
-while run:
-	while True:
-		os.system('cls')
-		try:
-			monitor_time = int(input("Please specify your monitor timing (time between each scan, in whole secondes!)"))
-			break
-		except ValueError:
-			continue
-	while True:
-		os.system('cls')
-		try:
-			print("Which unit of measurement would you like to use?")
-			print("1.byte")
-			print("2.MegaByte (MB)")
-			print("3.GigaByte (GB)")
-			measure_unit = int(input())
-			if 1<=measure_unit<=3:	
-				break
-			else:
-				continue
-		except ValueError:
-			print("Invalid input. Try again please.")
-			continue
-	daemon.start_daemon(monitor_time,measure_unit)		
-	data_sent = stats.data_extract('sent')
-	data_recv = stats.data_extract('recv')
-	stats.plot_bar(data_sent,data_recv,measure_unit)
+time.sleep(1.6)
+while True:
 	os.system('cls')
-	while True:
-		restart = input("Run again? y/n")
-		if restart == 'y':	
-			break
-		elif restart =='n':
-			run = False
+	print("Menu:")
+	print("1.Start monitoring")
+	print("2.Help")
+	print("3.Exit")
+	try:
+		choice = int(input())
+	except ValueError:
+		continue
+	if choice == 3:
+		run = False
+		try:
+			os.remove("data_recv.data")
+			os.remove("data_sent.data")
+		except OSError:
+			pass
+		print('Ciao!')
+		break
+	elif choice == 1:
+		run = True
+	elif choice == 2:
+		os.system('cls')
+		print("Help")
+		print("______")
+		print("""Bandwith monitor is an application which monitors your sent and recieved packets in timeouts of your choosing.
+It will then show you the results in a graph using your desired units of measurement.
+			""")
+		input("Press any key to continue...")
+		continue
+	else:
+		continue
+	if run:
+		while True:
 			os.system('cls')
-			break
-		else:
-			print("Y/N only.")
-			continue
-print('Ciao!')
+			try:
+				monitor_time = int(input("Please specify your monitor timing (time between each scan, in whole secondes!)"))
+				break
+			except ValueError:
+				continue
+		while True:
+			os.system('cls')
+			try:
+				print("Which unit of measurement would you like to use?")
+				print("1.byte")
+				print("2.MegaByte (MB)")
+				print("3.GigaByte (GB)")
+				measure_unit = int(input())
+				if 1<=measure_unit<=3:	
+					break
+				else:
+					continue
+			except ValueError:
+				print("Invalid input. Try again please.")
+				continue
+		daemon.start_daemon(monitor_time,measure_unit)		
+		data_sent = stats.data_extract('sent')
+		data_recv = stats.data_extract('recv')
+		stats.plot_bar(data_sent,data_recv,measure_unit)
+		os.system('cls')
+		
+	
